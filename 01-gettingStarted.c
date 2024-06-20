9f486d043e2cb28e9b7ce65fd12a833b5d1218eb
Linus <reza.linusx@gmail.com>
2024-06-20 17:31:16 +0330

    Create 01-gettingStarted.c

---
+++ 01-gettingStarted.c
@@ -0,0 +1,14 @@
+// this line almost always appears in the beginning of a C program
+// lets us work with input and output functions
+#include <stdio.h>
+
+// this function always appear in C program
+// the code inside this function will be executed
+int main()
+{
+    // this function is used to output/print text on the screen
+    printf("hello world");
+
+    // this line ends the main function
+    return 0;
+}
